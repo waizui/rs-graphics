927ef681495947aa04998aff1a746eab02b7f0bb
leo <lch01234@gmail.com>
2024-11-19 12:14:04 +0900

    delete: pfm

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,5 @@ pub mod haltonsampler;
 pub mod primes;
 pub mod randomizer;
 pub mod sampler;
-mod sobolmatrices;
+pub mod sobolmatrices;
 pub mod sobolsampler;
--- src/pfm_util.rs
+++
@@ -1,6 +0,0 @@
-/// pfm file
-struct PFM {}
-
-pub fn import_pfm(path: &str) -> PFM {
-    todo!()
-}
