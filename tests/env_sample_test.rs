d603e1304cb35ad8a382016769c9a14fb2313e51
leo <lch01234@gmail.com>
2024-12-02 16:17:58 +0900

    sync

--- tests/env_sample_test.rs
+++ tests/env_sample_test.rs
@@ -3,7 +3,7 @@ use rs_sampler::{haltonsampler::HaltonSampler, sampler::Sampler};
 
 type Image = PFM;
 type Real = f32;
-const PI:Real = std::f32::consts::PI;
+const PI: Real = std::f32::consts::PI;
 
 fn roundi(f: Real) -> i32 {
     (f + 0.5) as i32
@@ -13,9 +13,81 @@ fn tex2pixel(t: Real, num_p: usize) -> usize {
     roundi(t * (num_p as Real) - 0.5) as usize
 }
 
-fn calc_grayscale(path: &str) -> Image {
-    let pfm = PFM::read_from(path).unwrap();
+fn pixel2tex(x: usize, y: usize, img: &Image) -> [Real; 2] {
+    let u = x as Real / img.w as Real;
+    let v = y as Real / img.h as Real;
+    [u, v]
+}
 
+fn unitsphere2envmap(d: &[f32; 3]) -> [Real; 2] {
+    let x = d[0].abs();
+    let y = d[1].abs();
+    let z = d[2].abs();
+    let r = (1. - z).sqrt();
+    let phi = y.atan2(x);
+    let phi = phi * std::f32::consts::FRAC_2_PI;
+    let v = phi * r;
+    let u = r - v;
+    let (u, v) = if d[2] < 0. { (1. - v, 1. - u) } else { (u, v) };
+    let u = u.copysign(d[0]);
+    let v = v.copysign(-d[1]);
+    [u * 0.5 + 0.5, v * 0.5 + 0.5]
+}
+
+fn envmap2unitsphere(u: Real, v: Real) -> [Real; 3] {
+    let r = u - 0.5;
+    let phi = (PI / 4.) * (v - 0.5) / r;
+
+    let x = phi.cos() * r * (2. - r * r).sqrt();
+    let y = phi.sin() * r * (2. - r * r).sqrt();
+    let z = 1. - r * r;
+
+    [x, y, z]
+}
+
+fn get_color(u: Real, v: Real, img: &Image) -> [Real; 3] {
+    let channel = img.channels;
+    let i_w = tex2pixel(u, img.w);
+    let i_h = tex2pixel(v, img.w);
+
+    // let r = img.data[530360];
+    // let g = img.data[530361];
+    // let b = img.data[530362];
+    //
+    // dbg!(r,g,b);
+
+    let i = i_h * img.w + i_w;
+    if channel == 3 {
+        let r = img.data[i];
+        let g = img.data[i + 1];
+        let b = img.data[i + 2];
+        [r, g, b]
+    } else if channel == 1 {
+        let g = img.data[i];
+        [g, g, g]
+    } else {
+        panic!("Invalid channel");
+    }
+}
+
+fn set_color(col: &[Real; 3], u: Real, v: Real, img: &mut Image) {
+    let channel = img.channels;
+    let i_w = tex2pixel(u, img.w);
+    let i_h = tex2pixel(v, img.w);
+
+    let i = i_h * img.w + i_w;
+    if channel == 3 {
+        img.data[i] = col[0];
+        img.data[i + 1] = col[1];
+        img.data[i + 2] = col[2];
+    } else if channel == 1 {
+        img.data[i] = col[0];
+    } else {
+        panic!("Invalid channel");
+    }
+}
+
+fn calc_grayscale(pfm: &Image) -> Image {
     if pfm.channels < 3 {
         panic!("pfm channel invalid");
     }
@@ -49,9 +121,9 @@ fn calc_integral_over_grayscale(img: &Image) -> Real {
 
     let mut sum = 0.;
 
-    for i_w in 0..w {
-        for i_h in 0..h {
-            let gray = img.data[i_w * w + i_h];
+    for i_h in 0..h {
+        for i_w in 0..w {
+            let gray = img.data[i_h * w + i_w];
             sum += gray;
         }
     }
@@ -65,8 +137,8 @@ fn binary_find_inv_cdf() -> Real {
 }
 
 /// x,y: uniformal variables
-/// returns sampling position of x,y
-fn calc_sample_coord(x: Real, y: Real, img: &Image) -> (usize, usize) {
+/// returns sampling position of x,y and pdf
+fn calc_sample_coord(x: Real, y: Real, img: &Image, itgr: Real) -> ([usize; 2], Real) {
     let w = img.w;
     let h = img.h;
     let mut sum = 0.;
@@ -108,51 +180,53 @@ fn calc_sample_coord(x: Real, y: Real, img: &Image) -> (usize, usize) {
         }
     }
 
-    (i_x, i_y)
+    let pdf = img.data[i_h_fix * w + i_w_fix] / itgr;
+
+    ([i_x, i_y], pdf)
 }
 
-fn unitsphere2envmap(d: &[f32; 3]) -> [Real; 2] {
-    let x = d[0].abs();
-    let y = d[1].abs();
-    let z = d[2].abs();
-    let r = (1. - z).sqrt();
-    let phi = y.atan2(x);
-    let phi = phi * std::f32::consts::FRAC_2_PI;
-    let v = phi * r;
-    let u = r - v;
-    let (u, v) = if d[2] < 0. { (1. - v, 1. - u) } else { (u, v) };
-    let u = u.copysign(d[0]);
-    let v = v.copysign(-d[1]);
-    [u * 0.5 + 0.5, v * 0.5 + 0.5]
-}
-
-fn envmap2unitsphere(u: Real, v: Real) -> [Real; 3] {
-    let r = u;
-    let phi =  (PI/4.) * v/u
-
-    todo!()
-}
-
-fn sample_light(img: &Image) -> Image {
-    let w = img.w;
-    let h = img.h;
+fn sample_light(grayscale: &Image, envmap: &Image) -> Image {
+    let w = grayscale.w;
+    let h = grayscale.h;
 
     let mut res = PFM {
         data: vec![0.; w * h],
         w,
         h,
-        channels: 1,
-        little_endian: img.little_endian,
+        channels: 3,
+        little_endian: grayscale.little_endian,
     };
 
     let mut halton = HaltonSampler::new();
-    let itgr = calc_integral_over_grayscale(img);
-    for i in 0..1024 {
-        Sampler::<Real>::set_i(&mut halton, i);
-        Sampler::<Real>::set_dim(&mut halton, 0);
-        let xy: [Real; 2] = halton.get2d();
-        let st = calc_sample_coord(xy[0], xy[1], img);
-        res.data[st.0 * w + st.1] = 1.;
+    let itgr = calc_integral_over_grayscale(grayscale);
+
+    let nsamples = 8;
+    for i_w in 0..64 {
+        for i_h in 0..64 {
+            // screen coord to world
+            let tex = pixel2tex(i_w, i_h, grayscale);
+            let nrm = envmap2unitsphere(tex[0], tex[1]);
+
+            for i in 0..nsamples {
+                Sampler::<Real>::set_i(&mut halton, i + 1);
+                Sampler::<Real>::set_dim(&mut halton, 0);
+                let xy: [Real; 2] = halton.get2d();
+                // sampling position
+                let coord_pfd = calc_sample_coord(xy[0], xy[1], grayscale, itgr);
+                let st = coord_pfd.0;
+                let p2t = pixel2tex(st[0], st[1], grayscale);
+                // sampling ray direction
+                let raydir = envmap2unitsphere(p2t[0], p2t[1]);
+
+                let costheta = del_geo_core::vec3::dot(&raydir, &nrm);
+                if costheta > 0. {
+                    let mut color = get_color(p2t[0], p2t[1], envmap);
+                    let pdf = coord_pfd.1;
+                    set_color(&color, tex[0], tex[1], &mut res);
+                }
+                // costheta
+            }
+        }
     }
 
     res
@@ -168,7 +242,8 @@ fn sample_light_lookup(img: &Image, tbl: &[Image; 2]) -> Image {
 
 #[test]
 fn test_env_sample() {
-    let img = calc_grayscale("asset/envmap.pfm");
-    let env = sample_light(&img);
+    let pfm = PFM::read_from("asset/envmap.pfm").unwrap();
+    let grayscale = calc_grayscale(&pfm);
+    let env = sample_light(&grayscale, &pfm);
     let _ = env.save("target/envmap_gray.pfm");
 }
