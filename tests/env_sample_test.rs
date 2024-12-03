efcb0f264663f6d9a8ac132517699f1fde1be319
leo <lch01234@gmail.com>
2024-12-03 11:07:11 +0900

    feat:  sphere mapping

--- tests/env_sample_test.rs
+++ tests/env_sample_test.rs
@@ -1,4 +1,5 @@
 use del_raycast_core::io_pfm::PFM;
+use num_traits::abs;
 use rs_sampler::{haltonsampler::HaltonSampler, sampler::Sampler};
 
 type Image = PFM;
@@ -40,17 +41,27 @@ fn unitsphere2envmap(d: &[f32; 3]) -> [Real; 2] {
     let u = r - v;
     let (u, v) = if d[2] < 0. { (1. - v, 1. - u) } else { (u, v) };
     let u = u.copysign(d[0]);
-    let v = v.copysign(-d[1]);
+    let v = v.copysign(d[1]);
     [u * 0.5 + 0.5, v * 0.5 + 0.5]
 }
 
-fn envmap2unitsphere(u: Real, v: Real) -> [Real; 3] {
-    let r = u - 0.5;
-    let phi = (PI / 4.) * (v - 0.5) / r;
+// https://github.com/mmp/pbrt-v4/blob/1ae72cfa7344e79a7815a21ed3da746cdccee59b/src/pbrt/util/math.cpp#L292
+fn envmap2unitsphere(p: &[Real; 2]) -> [Real; 3] {
+    // map [0,1] to [-1,1]
+    let u = 2. * p[0] - 1.;
+    let v = 2. * p[1] - 1.;
+    let up = u.abs();
+    let vp = v.abs();
+    let sd = 1. - (up + vp);
+    let d = sd.abs();
+    let r = 1. - d;
+    let phi = (if r == 0. { 1. } else { (vp - up) / r + 1. }) * PI / 4.;
+    let z = (1. - r * r).copysign(sd);
+    let cosphi = phi.cos().copysign(u);
+    let sinphi = phi.sin().copysign(v);
 
-    let x = phi.cos() * r * (2. - r * r).sqrt();
-    let y = phi.sin() * r * (2. - r * r).sqrt();
-    let z = 1. - r * r;
+    let x = cosphi * r * (2. - r * r).sqrt();
+    let y = sinphi * r * (2. - r * r).sqrt();
 
     [x, y, z]
 }
@@ -119,15 +130,15 @@ fn calc_grayscale(pfm: &Image) -> Image {
     grayscale
 }
 
-fn calc_integral_over_grayscale(img: &Image) -> Real {
-    let w = img.w;
-    let h = img.h;
+fn calc_integral_over_grayscale(gray_img: &Image) -> Real {
+    let w = gray_img.w;
+    let h = gray_img.h;
 
     let mut sum = 0.;
 
     for i_h in 0..h {
         for i_w in 0..w {
-            let gray = img.data[i_h * w + i_w];
+            let gray = gray_img.data[i_h * w + i_w];
             sum += gray;
         }
     }
@@ -142,22 +153,22 @@ fn binary_find_inv_cdf() -> Real {
 
 /// x,y: uniformal variables
 /// returns sampling position of x,y and pdf
-fn calc_sample_coord(x: Real, y: Real, img: &Image, itgr: Real) -> ([usize; 2], Real) {
-    let w = img.w;
-    let h = img.h;
+fn calc_sample_coord(x: Real, y: Real, gray_img: &Image, itgr: Real) -> ([usize; 2], Real) {
+    let w = gray_img.w;
+    let h = gray_img.h;
     let mut sum = 0.;
 
     let mut r_avg = 0.;
     let i_h_fix = tex2pixel(y, h);
     for i_w in 0..w {
-        let gray = img.data[i_h_fix * w + i_w];
+        let gray = gray_img.data[i_h_fix * w + i_w];
         r_avg += gray;
     }
     r_avg /= w as Real;
 
     let mut i_x = 0; // sampling pixel coord of variable y
     for i_w in 0..w {
-        let gray = img.data[i_h_fix * w + i_w];
+        let gray = gray_img.data[i_h_fix * w + i_w];
         sum += gray / (r_avg * w as Real); // TODO: r_avg ==0
         if sum >= x {
             i_x = i_w;
@@ -168,7 +179,7 @@ fn calc_sample_coord(x: Real, y: Real, img: &Image, itgr: Real) -> ([usize; 2], 
     let mut c_avg = 0.;
     let i_w_fix = tex2pixel(x, w); // at given y
     for i_h in 0..h {
-        let gray = img.data[i_h * w + i_w_fix];
+        let gray = gray_img.data[i_h * w + i_w_fix];
         c_avg += gray;
     }
     c_avg /= h as Real;
@@ -176,7 +187,7 @@ fn calc_sample_coord(x: Real, y: Real, img: &Image, itgr: Real) -> ([usize; 2], 
     sum = 0.;
     let mut i_y = 0;
     for i_h in 0..h {
-        let gray = img.data[i_h * w + i_w_fix];
+        let gray = gray_img.data[i_h * w + i_w_fix];
         sum += gray / (c_avg * h as Real);
         if sum >= y {
             i_y = i_h;
@@ -184,18 +195,18 @@ fn calc_sample_coord(x: Real, y: Real, img: &Image, itgr: Real) -> ([usize; 2], 
         }
     }
 
-    let pdf = img.data[i_h_fix * w + i_w_fix] / itgr;
+    let pdf = gray_img.data[i_h_fix * w + i_w_fix] / itgr;
 
     ([i_x, i_y], pdf)
 }
 
 fn sample_light(grayscale: &Image, envmap: &Image) -> Image {
-    let w = 128;
-    let h = 128;
+    let w = 64;
+    let h = 64;
 
-    let debug = true;
+    let debug = false;
 
-    let mut res = create_image(3, w, h);
+    let mut img = create_image(3, w, h);
 
     let mut halton = HaltonSampler::new();
     let itgr = calc_integral_over_grayscale(grayscale);
@@ -205,9 +216,9 @@ fn sample_light(grayscale: &Image, envmap: &Image) -> Image {
         for i_h in 0..h {
             // screen coord to world
             let tex = pixel2tex(i_w, i_h, w, h);
-            let nrm = envmap2unitsphere(tex[0], tex[1]);
+            let nrm = envmap2unitsphere(&tex);
 
-            let mut color = [0.; 3];
+            let mut resut = [0.; 3];
             for i in 0..nsamples {
                 Sampler::<Real>::set_i(&mut halton, i + 1);
                 Sampler::<Real>::set_dim(&mut halton, 0);
@@ -215,37 +226,43 @@ fn sample_light(grayscale: &Image, envmap: &Image) -> Image {
                 // sampling position
                 let coord_pfd = calc_sample_coord(random[0], random[1], grayscale, itgr);
                 let st = coord_pfd.0;
+                let pdf = coord_pfd.1;
                 let s_tex_coord = pixel2tex(st[0], st[1], grayscale.w, grayscale.h);
                 // sampling ray direction
-                let raydir = envmap2unitsphere(s_tex_coord[0], s_tex_coord[1]);
-
-                let mut tex_color = get_color(s_tex_coord[0], s_tex_coord[1], envmap);
+                let raydir = envmap2unitsphere(&s_tex_coord);
+                let radiance = get_color(s_tex_coord[0], s_tex_coord[1], envmap);
                 if debug {
-                    set_color(&[1., 0., 0.], s_tex_coord[0], s_tex_coord[1], &mut res);
+                    set_color(&[1., 0., 0.], s_tex_coord[0], s_tex_coord[1], &mut img);
                 } else {
                     let costheta = del_geo_core::vec3::dot(&raydir, &nrm);
-                    if costheta > 0. {
-                        del_geo_core::vec3::scale(&mut tex_color, 0.3);
-                        color = del_geo_core::vec3::add(&color, &tex_color);
-                        let pdf = coord_pfd.1;
+                    if costheta > 0. && pdf > 0. {
+                        let sintheta = raydir[2];
+                        let mut color = [PI * 2.; 3]; //Jacobian TODO:replace with correct term
+                        color[0] *= radiance[0];
+                        color[1] *= radiance[1];
+                        color[2] *= radiance[2];
+                        del_geo_core::vec3::scale(&mut color, costheta);
+                        del_geo_core::vec3::scale(&mut color, sintheta);
+                        del_geo_core::vec3::scale(&mut color, 1. / pdf);
+                        resut = del_geo_core::vec3::add(&resut, &radiance);
                     }
                 }
             }
 
             if debug {
-                let color = get_color(tex[0], tex[1], &res);
+                let color = get_color(tex[0], tex[1], &img);
                 if color[0] < 1. {
                     let tex_color = get_color(tex[0], tex[1], envmap);
-                    set_color(&tex_color, tex[0], tex[1], &mut res);
+                    set_color(&tex_color, tex[0], tex[1], &mut img);
                 }
             } else {
-                del_geo_core::vec3::scale(&mut color, 1. / (nsamples as Real));
-                set_color(&color, tex[0], tex[1], &mut res);
+                del_geo_core::vec3::scale(&mut resut, 1. / (nsamples as Real));
+                set_color(&resut, tex[0], tex[1], &mut img);
             }
         }
     }
 
-    res
+    img
 }
 
 fn calc_cdf_inv_lookup(img: &Image) -> [Image; 2] {
@@ -254,6 +271,24 @@ fn calc_cdf_inv_lookup(img: &Image) -> [Image; 2] {
 
 fn sample_light_lookup(img: &Image, tbl: &[Image; 2]) -> Image {
     todo!()
+}
+
+#[test]
+fn test_sphere_mapping() {
+    let mut dir = [0.; 3];
+
+    for i in 0..3 {
+        dir[i] = (i + 1) as Real;
+    }
+
+    del_geo_core::vec3::normalize(&mut dir);
+
+    let uv = unitsphere2envmap(&dir);
+    let udir = envmap2unitsphere(&uv);
+
+    assert!(dir[0] - udir[0] < 0.001);
+    assert!(dir[1] - udir[1] < 0.001);
+    assert!(dir[2] - udir[2] < 0.001);
 }
 
 #[test]
